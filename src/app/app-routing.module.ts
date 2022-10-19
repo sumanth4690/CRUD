0badd77a39841f03a3542d6714eeeca7dc4e4033
Thallapelly Sumanth <tsumanth@ariqt.com>
2022-10-19 15:12:24 +0530

    code otimized

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './components/add/add.component';
+import { ChildComponent } from './components/child/child.component';
+import { Child2Component } from './components/child2/child2.component';
+import { EditComponent } from './components/edit/edit.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotAvilableComponent } from './components/not-avilable/not-avilable.component';
+import { ReactiveComponent } from './components/reactive/reactive.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  { path: 'child', component: ChildComponent },
+  { path: 'child2', component: Child2Component },
+  { path: 'add', component: AddComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'home/:id', component: UserDetailsComponent },
+  {path: 'home/:id/edit',component:EditComponent},
+  {path:'reactiveForm',component:ReactiveComponent},
+  {path:'**', component:NotAvilableComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
+export const routings = [
+  Child2Component,
+  ChildComponent,
+  AddComponent,
+  HomeComponent,
+  EditComponent,
+  UserDetailsComponent,
+];
---
+++ src/app/components/user-details/user-details.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EventHandlerService } from 'src/app/event-handler.service';
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.scss']
+})
+export class UserDetailsComponent implements OnInit {
+  id: any;
+  userDetails: any;
+  loading: boolean=true;
+  apiData: any;
+  data:any=[];
+  editMode=true;
+
+  constructor(private _activatedRoute:ActivatedRoute, private _eh:EventHandlerService, private _router:Router
+    ) { }
+
+  ngOnInit(): void {
+    this.id = this._activatedRoute.snapshot.paramMap.get('id');
+    this._eh.getData().subscribe((data) => {
+      this.apiData = data;
+      this.userDetails = this.GetById(this.id);
+      this.loading = false;
+      this.getUsers();
+    });
+  }
+  getUsers(){
+    this._eh.getData().subscribe((value:any)=>this.data=value)
+  }
+  private findIndex = (id: number) =>
+    this.apiData.findIndex((x: any) => x.id == id);
+
+  GetById(id: any) {
+    const index = this.findIndex(id);
+    return this.apiData[index];
+  }
+  EditUser(id:any){
+    this._router.navigate(['/home',id,'edit']);
+
+  }
+  update() {
+    let index = this.findIndex(this.id);
+    this._eh.userDetails[index] = this.userDetails;
+    this._router.navigate(['home']);
+  }
+  goBack(){
+    this._router.navigate(['home'])
+  }
+}
